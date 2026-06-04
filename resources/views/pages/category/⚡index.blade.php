68c92245912c2688270dbee690ba175121def067
Mukhlis-AMJ <jaelaniabdulmukhlis@gmail.com>
2026-06-04 18:29:19 +0800

    update red

--- resources/views/pages/category/⚡index.blade.php
+++ resources/views/pages/category/⚡index.blade.php
@@ -25,7 +25,7 @@ new class extends Component
             <flux:table.cell>Jul 29, 10:45 AM</flux:table.cell>
             <flux:table.cell><flux:badge color="green" size="sm" inset="top bottom">Paid</flux:badge></flux:table.cell>
             <flux:table.cell variant="strong">$49.00</flux:table.cell>
-            <flux:table.cell><flux:badge color="blue" size="sm" inset="top bottom">Action</flux:badge></flux:table.cell>
+            <flux:table.cell><flux:badge color="green" size="sm" inset="top bottom">Action</flux:badge></flux:table.cell>
         </flux:table.row>
 
         <flux:table.row>
@@ -33,7 +33,7 @@ new class extends Component
             <flux:table.cell>Jul 28, 2:15 PM</flux:table.cell>
             <flux:table.cell><flux:badge color="green" size="sm" inset="top bottom">Paid</flux:badge></flux:table.cell>
             <flux:table.cell variant="strong">$312.00</flux:table.cell>
-            <flux:table.cell><flux:badge color="blue" size="sm" inset="top bottom">Fantasi</flux:badge></flux:table.cell>
+            <flux:table.cell><flux:badge color="green" size="sm" inset="top bottom">Fantasi</flux:badge></flux:table.cell>
         </flux:table.row>
 
         <flux:table.row>
@@ -41,7 +41,7 @@ new class extends Component
             <flux:table.cell>Jul 30, 4:05 PM</flux:table.cell>
             <flux:table.cell><flux:badge color="zinc" size="sm" inset="top bottom">Refunded</flux:badge></flux:table.cell>
             <flux:table.cell variant="strong">$132.00</flux:table.cell>
-            <flux:table.cell><flux:badge color="blue" size="sm" inset="top bottom">Romance</flux:badge></flux:table.cell>
+            <flux:table.cell><flux:badge color="red" size="sm" inset="top bottom">Romance</flux:badge></flux:table.cell>
         </flux:table.row>
 
         <flux:table.row>
@@ -49,7 +49,7 @@ new class extends Component
             <flux:table.cell>Jul 27, 9:30 AM</flux:table.cell>
             <flux:table.cell><flux:badge color="green" size="sm" inset="top bottom">Paid</flux:badge></flux:table.cell>
             <flux:table.cell variant="strong">$31.00</flux:table.cell>
-            <flux:table.cell><flux:badge color="blue" size="sm" inset="top bottom">Horror</flux:badge></flux:table.cell>
+            <flux:table.cell><flux:badge color="green" size="sm" inset="top bottom">Horror</flux:badge></flux:table.cell>
         </flux:table.row>
         
         <flux:table.row>
@@ -57,7 +57,7 @@ new class extends Component
             <flux:table.cell>Mei 15, 7:35 AM</flux:table.cell>
             <flux:table.cell><flux:badge color="yellow" size="sm" inset="top bottom">Done</flux:badge></flux:table.cell>
             <flux:table.cell variant="strong">$31.00</flux:table.cell>
-            <flux:table.cell><flux:badge color="blue" size="sm" inset="top bottom">Comedy</flux:badge></flux:table.cell>
+            <flux:table.cell><flux:badge color="green" size="sm" inset="top bottom">Comedy</flux:badge></flux:table.cell>
         </flux:table.row>
     </flux:table.rows>
 </flux:table>
